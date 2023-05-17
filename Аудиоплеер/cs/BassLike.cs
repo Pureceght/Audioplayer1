283264cc583433684274e3ecf6349d596a706819
Pureceght <maksimaikido@mail.ru>
2023-05-18 03:50:25 +0500

    BassLike complete
    Файл с методами и переменными

--- Аудиоплеер/cs/BassLike.cs
+++ Аудиоплеер/cs/BassLike.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Un4seen.Bass;
 
 namespace Аудиоплеер
 {
@@ -12,20 +13,89 @@ namespace Аудиоплеер
         /// Частота дискретизации
         /// </summary>
         private static int HZ = 44100;
-
         /// <summary>
         /// Состояние инициализации
         /// </summary>
         public static bool InitDefaultDevice;
-
         /// <summary>
         /// Главный поток плеера (канал)
         /// </summary>
         public static int Stream;
-
         /// <summary>
         /// Громкость
         /// </summary>
         public static int Volume = 100;
+        //^^^ПЕРЕМЕННЫЕ^^^//
+        private static bool InitBass(int hz)
+        {
+            if (!InitDefaultDevice)
+                InitDefaultDevice = Bass.BASS_Init(-1, hz, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+            return InitDefaultDevice;
+        }
+        /// <summary>
+        /// Воспроизведение
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="vol"></param>
+        public static void Play(string filename, int vol)
+        {
+            Stop();
+            if (InitBass(HZ))
+            {
+                Stream = Bass.BASS_StreamCreateFile(filename, 0, 0, BASSFlag.BASS_DEFAULT);
+                if(Stream != 0)
+                {
+                    Volume = vol;
+                    Bass.BASS_ChannelSetAttribute(Stream, BASSAttribute.BASS_ATTRIB_VOL, Volume / 100);
+                    Bass.BASS_ChannelPlay(Stream, false);
+                }
+            }
+        }
+        /// <summary>
+        /// Стоп
+        /// </summary>
+        public static void Stop()
+        {
+            Bass.BASS_ChannelStop(Stream);
+            Bass.BASS_StreamFree(Stream);
+        }
+        /// <summary>
+        /// Получение длительности канала в секундах
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static int GetTimeOfStream(int stream)
+        {
+            long TimeBytes = Bass.BASS_ChannelGetLength(stream);
+            double Time = Bass.BASS_ChannelBytes2Seconds(stream, TimeBytes);
+            return (int)Time;
+        }
+        /// <summary>
+        /// Получение текущей позиции в секундах
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static int GetPosOfStream(int stream)
+        {
+            long pos = Bass.BASS_ChannelGetPosition(stream);
+            int posSec = (int)Bass.BASS_ChannelBytes2Seconds(stream, posSec);
+            return posSec;
+        }
+
+        public static void SetPosOfScroll(int stream, int pos)
+        {
+            Bass.BASS_ChannelSetPosition(stream, (double)pos);
+        }
+
+        /// <summary>
+        /// Установка громкости
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="vol"></param>
+        public static void SetVolumeToStream(int stream, int vol)
+        {
+            Volume = vol;
+            Bass.BASS_ChannelSetAttribute(stream, BASSAttribute.BASS_ATTRIB_VOL, Volume / 100F);
+        }
     }
 }
