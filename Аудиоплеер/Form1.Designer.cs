f082ad332857ff3a01bb8c50b27db2402a2e5ea2
Pureceght <maksimaikido@mail.ru>
2023-05-18 01:20:33 +0500

    minor changes
    - изменения положения экрана

--- Аудиоплеер/Form1.Designer.cs
+++ Аудиоплеер/Form1.Designer.cs
@@ -34,8 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(832, 453);
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Аудиоплеер";
             this.ResumeLayout(false);
 
